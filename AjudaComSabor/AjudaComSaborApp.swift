ffaf3a94d1db3d41598ef06d91a7ae51e63b2452
Sara Lima <saralima06201234@gmail.com>
2023-06-16 16:58:22 -0300

    Initial Commit

---
+++ AjudaComSabor/AjudaComSaborApp.swift
@@ -0,0 +1,17 @@
+//
+//  AjudaComSaborApp.swift
+//  AjudaComSabor
+//
+//  Created by user on 16/06/23.
+//
+
+import SwiftUI
+
+@main
+struct AjudaComSaborApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
